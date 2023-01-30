99fc01f07b824bffb544efc144d4d04828ae02ee
Alejandro <a.daviidlopez@gmail.com>
2023-01-30 14:14:30 +0100

    Update view.bat

--- AntiV/hamburguesa/view.bat
+++ AntiV/hamburguesa/view.bat
@@ -1,4 +1,5 @@
 @echo off
+set num_aula=34
 set path=%path%;"C:\Program Files\Veyon"
-set /p num=192.168.34.
-veyon-cli remoteaccess view 192.168.34.%num%
+set /p ip_pc=192.168.%num_aula%.
+veyon-cli remoteaccess view 192.168.34.%ip_pc%
